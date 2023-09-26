20ae25e5755b810c18034d5c84fd17de41432f1b
Shu2M <shulgame@bk.ru>
2023-09-26 15:27:32 +0300

    set kfile commands

--- commands.py
+++ commands.py
@@ -6,6 +6,10 @@
 import abc
 import typing
 
+import config
+
+CONFIG_FILE = config.ConfigTxt('config.txt')
+
 
 class Command(abc.ABC):
     """Абстрактный класс комманды.
@@ -30,3 +34,40 @@ class Command(abc.ABC):
         raise NotImplementedError(
             'Наследник класса Command должен реализовать метод execute',
         )
+
+
+class SetKeyFilePathCommand(Command):
+    """Команда установки пути до k файла."""
+
+    def execute(
+        self,
+        additional_data: typing.Any,
+    ):
+        """Метод исполнения команды.
+
+        Args:
+            additional_data: строка путь до k файла
+
+        Returns:
+            статус, результат команды
+        """
+        CONFIG_FILE.update(keyfile_path=additional_data)
+        return True, None
+
+
+class ShowKeyFilePathCommand(Command):
+    """Команда показа пути до k файла."""
+
+    def execute(
+        self,
+        additional_data: typing.Any,
+    ):
+        """Метод исполнения команды.
+
+        Args:
+            additional_data: дополнительные данные (не требуются)
+
+        Returns:
+            статус, результат команды
+        """
+        return True, CONFIG_FILE.read('keyfile_path')
