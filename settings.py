b64c2f8e40235f217ba7358ba71a66aaa732ad7f
Shu2M <shulgame@bk.ru>
2023-09-27 16:55:54 +0300

    logger formatter string

--- settings.py
+++ settings.py
@@ -6,3 +6,9 @@ import config
 BASE_DIR = Path(__file__).resolve().parent
 CONFIG_FILE = config.ConfigTxt(str(BASE_DIR / 'config.txt'))
 LOGGER_FILE_PATH = BASE_DIR / 'log.log'
+LOGGER_FORMATTER_STRING = (
+    '%(asctime)s - '
+    + '[%(levelname)s] - '
+    + '(%(funcName)s%(lineno)d) - '
+    + '%(message)s'
+)
