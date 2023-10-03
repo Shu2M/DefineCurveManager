fa2f4a8161a19c05cb20b18866044e96fde434d6
Shu2M <shulgame@bk.ru>
2023-10-03 15:56:36 +0300

    add command in menu

--- source/menus/NewCurveMenu.py
+++ source/menus/NewCurveMenu.py
@@ -1,7 +1,10 @@
 """Меню добавления новой curve в keyfile."""
-from source.menus.Menu import Menu
+from collections import namedtuple
+
+from source.menus.Menu import Menu, get_user_input
 from source.Option import Option
 from source.commands.BackMenuCommand import BackMenuCommand
+from source.commands.NewCurveByRangeCommand import NewCurveByRangeCommand
 
 
 class NewCurveMenu(Menu):
@@ -11,9 +14,27 @@ class NewCurveMenu(Menu):
         """Метод инициализации."""
         super().__init__()
         self.options = {
+            1: Option(
+                name='По диапазону',
+                command=NewCurveByRangeCommand(),
+                prep_call=get_user_range,
+                success_message='Новая curve добавлена',
+            ),
             4: Option(
                 name='Назад',
                 command=BackMenuCommand(),
                 success_message='',
             ),
         }
+
+
+def get_user_range():
+    """Функция возвращает необходимые данные для работы команды."""
+    Range = namedtuple('Range', 'name lcid start stop step')
+    return Range(
+        get_user_input('Имя новой curve'),
+        get_user_input('id новой кривой', required_type=int),
+        get_user_input('Начало диапазона', required_type=int),
+        get_user_input('Конец диапазона', required_type=int),
+        get_user_input('Шаг', required_type=int),
+    )
