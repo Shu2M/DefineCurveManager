4407f131ff04644324ec1b96b221e201bcfc565c
Shu2M <shulgame@bk.ru>
2023-10-03 11:19:54 +0300

    extend get_user_input realisation

--- source/menus/Menu.py
+++ source/menus/Menu.py
@@ -4,6 +4,7 @@
 """
 import os
 import sys
+import typing
 
 from source import exceptions
 import settings
@@ -84,46 +85,57 @@ def select_option(options: dict):
     Returns:
         объект выбранной опции
     """
-    option_key = try_get_user_input_as_int('Выберете вариант действия: ')
+    option_key = try_get_user_required_type_input(
+        'Выберете вариант действия: ',
+        required_type=int,
+    )
     while option_key not in options.keys():
         sys.stdout.write('\033[F\033[K')
-        option_key = try_get_user_input_as_int(
+        option_key = try_get_user_required_type_input(
             'Выбрана несуществующая опция. Выберете другой вариант: ',
+            required_type=int,
         )
     sys.stdout.write('\n')
     return options[option_key]
 
 
-def try_get_user_input_as_int(msg: str) -> int | str:
-    """Функция пробует перевести пользовательский ввод в int.
-
-    Args:
-        msg: сообщение, выводимое при вводе
-
-    Returns:
-         пользовательский ввод типа int если его можно преобразовать иначе str
-    """
-    user_input = input(msg)
-    try:
-        return int(user_input)
-    except ValueError:
-        return user_input
-
-
-def get_user_input(label, required=True) -> str:
-    """Функция пользовательского ввод.
+def get_user_input(label, required=True, required_type=str) -> typing.Any:
+    """Функция пользовательского ввода.
 
     Args:
         label: выводимое собщение перед вводом
         required: флаг окончания пользователького ввода
+        required_type: тип, к которому следует привести пользовательский ввод
 
     Returns:
         значение пользовательского ввода в виде строки
     """
-    user_input = input('{label}: '.format(label=label)) or None
-    while required and not user_input:
+    user_input = try_get_user_required_type_input(
+        '{label}: '.format(label=label),
+        required_type=required_type,
+    )
+    while required and not (user_input and isinstance(user_input, required_type)):
         sys.stdout.write('\033[F\033[K')
-        user_input = input(
-            'Ввод не выполнен. {label}: '.format(label=label),
-        ) or None
+        user_input = try_get_user_required_type_input(
+            'Ввод не выполнен или нельзя преобразовать '
+            + 'в требуемый тип. {label}: '.format(label=label),
+            required_type=required_type,
+        )
     return user_input
+
+
+def try_get_user_required_type_input(msg, required_type: typing.Type) -> typing.Any:
+    """Функция пробует перевести пользовательский ввод в required_type.
+
+        Args:
+            msg: сообщение, выводимое при вводе
+            required_type: требуемый тип ввода
+
+        Returns:
+             значение ввода в требуемом типе
+        """
+    user_input = input(msg)
+    try:
+        return required_type(user_input)
+    except ValueError:
+        return user_input
