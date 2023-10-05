31de9c58f35810c5326e91e11d2c53fcf2cfb4e0
Shu2M <shulgame@bk.ru>
2023-10-05 16:17:56 +0300

    add name and data_below_name properties realisation

--- source/keywords/Define/Curve.py
+++ source/keywords/Define/Curve.py
@@ -5,20 +5,67 @@ from source.keywords.KeywordAbstract import KeywordAbstract
 class Curve(KeywordAbstract):
     """Класс кейворда DEFINE_CURVE_(TITLE)."""
 
+    default_name = 'DEFINE_CURVE'
+    title: str = ''
+    lcid: int = 0
+    sidr: int = 0
+    sfa: float = 0.0
+    sfo: float = 0.0
+    offa: float = 0.0
+    offo: float = 0.0
+    dattyp: int = 0
+    lcint: int = 0
+    a1: list[int | float] = []
+    o1: list[int | float] = []
+
     def __init__(self):
         """Метод инициализации объекта."""
         pass
 
-    def __str__(self):
-        """Метод приведения объекта к типу str."""
-        return None
-
     @property
     def name(self):
         """Свойство, возвращающее имя кейворда."""
-        return None
+        result_name = self.default_name
+        if self.title:
+            result_name += '_TITLE'
+        return result_name
 
     @property
     def data_below_name(self):
         """Свойство, возвращающее данные, ниже имени кейворда, как строку."""
-        return None
+        result = ''
+
+        if self.title:
+            result += self.title + '\n'
+
+        result += '$#    lcid      sidr       sfa       sfo      offa      ' \
+                  'offo    dattyp     lcint\n'
+        for param_value in [
+            self.lcid,
+            self.sidr,
+            self.sfa,
+            self.sfo,
+            self.offa,
+            self.offo,
+            self.dattyp,
+            self.lcint,
+        ]:
+            result += self.format_param_value_to_string(
+                param_value=param_value,
+                string_cell_width=10,
+            )
+        result += '\n'
+
+        result += '$#                a1                  o1\n'
+        for a1, o1 in zip(self.a1, self.o1):
+            result += self.format_param_value_to_string(
+                param_value=a1,
+                string_cell_width=20,
+            )
+            result += self.format_param_value_to_string(
+                param_value=o1,
+                string_cell_width=20,
+            )
+            result += '\n'
+
+        return result.strip()
