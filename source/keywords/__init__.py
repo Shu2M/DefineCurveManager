751f9448423cbd5db37391c13d3db1899b1460cc
Shu2M <shulgame@bk.ru>
2023-10-05 14:51:25 +0300

    add package __init__ file for explicit import

---
+++ source/keywords/__init__.py
@@ -0,0 +1,5 @@
+"""модуль для импортирования классов в пространство имен source.keywords.
+
+Нужно для удобного и ясного импорта в модуле keywords-dispatch_dict.py
+"""
+from source.keywords.Define.Curve import Curve as DefineCurve
