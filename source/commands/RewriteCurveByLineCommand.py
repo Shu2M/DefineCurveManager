8220a5e93e678c3ae88cfdbc30a93b27755d9c21
Shu2M <shulgame@bk.ru>
2023-10-10 00:12:19 +0300

    add new command

---
+++ source/commands/RewriteCurveByLineCommand.py
@@ -0,0 +1,37 @@
+"""Команда перезаписи curve по построчному пользовательскому вводу."""
+import typing
+import re
+
+import settings
+from source.commands.Command import Command
+from source.Keyfile import Keyfile
+from source.input_output_interface import get_input_data_by_line
+
+
+class RewriteCurveByLineCommand(Command):
+    """Комманда перезаписи curve по построчному вводу."""
+
+    def execute(
+        self,
+        additional_data: typing.Any,
+    ):
+        """Метод исполнения команды.
+
+        Args:
+            additional_data: именованый кортеж с атрибутами lcid
+
+        Returns:
+            статус, результат команды
+        """
+        with Keyfile(settings.CONFIG_FILE.read('keyfile_path')) as keyfile:
+            for keyword in keyfile.keywords:
+                if re.match(r'DEFINE_CURVE', keyword.name):
+                    if keyword.lcid == additional_data.lcid:
+                        keyword.a1 = []
+                        keyword.o1 = []
+                        curve_data = get_input_data_by_line()
+                        for a1, o1 in curve_data:
+                            keyword.a1.append(a1)
+                            keyword.o1.append(o1)
+                        break
+        return True, None
