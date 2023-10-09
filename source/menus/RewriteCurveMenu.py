68afd77eed53554f207cfb53ddac15f0ad402f76
Shu2M <shulgame@bk.ru>
2023-10-10 00:13:20 +0300

    add new commands

--- source/menus/RewriteCurveMenu.py
+++ source/menus/RewriteCurveMenu.py
@@ -2,6 +2,9 @@
 from source.menus.Menu import Menu
 from source.Option import Option
 from source.commands.BackMenuCommand import BackMenuCommand
+from source.commands.RewriteCurveByRangeCommand import RewriteCurveByRangeCommand
+from source.commands.RewriteCurveByLineCommand import RewriteCurveByLineCommand
+from source.input_output_interface import get_parameterized_user_input_function
 
 
 class RewriteCurveMenu(Menu):
@@ -11,7 +14,26 @@ class RewriteCurveMenu(Menu):
         """Метод инициализации."""
         super().__init__()
         self.options = {
-            4: Option(
+            1: Option(
+                name='По диапазону',
+                command=RewriteCurveByRangeCommand(),
+                prep_call=get_parameterized_user_input_function(
+                    lcid=('id кривой', int),
+                    start=('Начало диапазона', int),
+                    stop=('Конец диапазона', int),
+                    step=('Шаг', int),
+                ),
+                success_message='Curve перезаписана',
+            ),
+            2: Option(
+                name='Построчный ввод',
+                command=RewriteCurveByLineCommand(),
+                prep_call=get_parameterized_user_input_function(
+                    lcid=('id кривой', int),
+                ),
+                success_message='Curve перезаписана',
+            ),
+            3: Option(
                 name='Назад',
                 command=BackMenuCommand(),
                 success_message='',
